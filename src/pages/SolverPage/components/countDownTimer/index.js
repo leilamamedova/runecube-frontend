5c077514c2f28e7a9b27c98b27e776caaa5c3e49
leilamamedova <leila.m26ova@gmail.com>
2022-03-16 01:13:47 +0400

    main and solver pages design fix 1

---
+++ src/pages/SolverPage/components/countDownTimer/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const CountDownTimer = ({minSecs}) => {
+   
+    const { minutes = 0, seconds = 60 } = minSecs;
+    const [[mins, secs], setTime] = React.useState([minutes, seconds]);
+    
+
+    const tick = () => {   
+        if (mins === 0 && secs === 0)             
+            setTime([0, 0]);
+         else if (secs === 0) {
+            setTime([mins - 1, 59]);
+        } else {
+            setTime([mins, secs - 1]);
+        }
+    };
+
+
+    const reset = () => setTime([parseInt(minutes), parseInt(seconds)]);
+
+    
+    React.useEffect(() => {
+        const timerId = setInterval(() => tick(), 1000);
+        return () => clearInterval(timerId);
+    });
+
+    
+    return (
+        <div>
+            <p>{`${mins
+            .toString()
+            .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</p> 
+        </div>
+    );
+}
+
+export default CountDownTimer;
--- src/pages/SolverPage/components/cube/index.js
+++ src/pages/SolverPage/components/cube/index.js
@@ -1,11 +1,69 @@
 import * as React from 'react';
-import './index.css'
 import Cube from 'react-3d-cube';
+import CountDownTimer from '../countDownTimer';
+import './index.scss'
+
+const data = [
+    {
+        index: 1,
+        timer: {
+            minutes: 3,
+            seconds: 0
+        },
+        count: 3,
+        image: 'https://res.cloudinary.com/dorehapc1/image/upload/v1647374901/rune_bqry8g.png'
+    },
+    {
+        index: 2,
+        timer: {
+            minutes: 10,
+            seconds: 3
+        },
+        count: 3,
+        image: 'https://res.cloudinary.com/dorehapc1/image/upload/v1647374901/rune_bqry8g.png'
+    },
+    {
+        index: 3,
+        timer: {
+            minutes: 10,
+            seconds: 3
+        },
+        count: 3,
+        image: 'https://res.cloudinary.com/dorehapc1/image/upload/v1647374901/rune_bqry8g.png'
+    },
+    {
+        index: 4,
+        timer: {
+            minutes: 10,
+            seconds: 3
+        },
+        count: 3,
+        image: 'https://res.cloudinary.com/dorehapc1/image/upload/v1647374901/rune_bqry8g.png'
+    },
+    {
+        index: 5,
+        timer: {
+            minutes: 10,
+            seconds: 3
+        },
+        count: 3,
+        image: 'https://res.cloudinary.com/dorehapc1/image/upload/v1647374901/rune_bqry8g.png'
+    },
+    {
+        index: 6,
+        timer: {
+            minutes: 10,
+            seconds: 3
+        },
+        count: 3,
+        image: 'https://res.cloudinary.com/dorehapc1/image/upload/v1647374901/rune_bqry8g.png'
+    },
+]
  
 class LogoCube extends React.Component {
   render() {
     return (
-    <center>
+        <center>
             <div>
                 <div
                     style={{
@@ -13,26 +71,18 @@ class LogoCube extends React.Component {
                         height: 300
                     }}
                 >
-                <Cube size={300} index="front">
-                    <div style={{backgroundColor: 'black'}}>
-                        <img style={{width: '280px', height: '300px'}} src="https://cdn.sanity.io/images/0vv8moc6/dvm360/0efdfab43ab36c3432ced2ceb2d52daae6a93c96-500x500.jpg"></img>
-                    </div>
-                    <div style={{backgroundColor: 'black'}}>
-                        <img style={{width: '280px', height: '300px'}} src="https://sitterforyourcritter.com/wp-content/uploads/2019/09/female-cat-1.jpg"></img>
-                    </div>
-                    <div style={{backgroundColor: 'black'}}>
-                        <img style={{width: '280px', height: '300px'}} src="https://pawsitivepotential.com/wp-content/uploads/2016/05/Tongue-Kitten-500x500_t.jpg"></img>
-                    </div>
-                    <div style={{backgroundColor: 'black'}}>
-                    <img style={{width: '280px', height: '300px'}} src="https://9ed48207422fa7fc5013-a6297eb5ec0f30e883355c8680f3b2d6.ssl.cf2.rackcdn.com/Hunter_wubba-9294%20(1)-20180803203739-20180803203910-500x500.jpg"></img>
-                    </div>
-                    <div style={{backgroundColor: 'black'}}>
-                    <img style={{width: '280px', height: '300px'}} src="https://www.guildinsurance.com.au/images/librariesprovider3/breed-images/500x500/cat_russian-blue-tica.jpg?sfvrsn=d16600b_2"></img>
-                    </div>
-                    <div style={{backgroundColor: 'black'}}>
-                    <img style={{width: '280px', height: '300px'}} src="https://www.catmospherecafe.com/assets/chiangmai-3ede22d062b60dde4a501695a2e0bfc7451f44d412ed68bd01cb6b44216270e4.jpg"></img>
-                    </div>
-                </Cube>
+                    <Cube size={300} index="front">
+                        {
+                            data.map(item => (
+                                <div key={item.index} className='cube-content'>
+                                    <CountDownTimer minSecs={item.timer}/>
+                                    <p>Count: {item.count}</p>
+                                    <img src={item.image}></img>
+                                </div>
+                                )
+                            )
+                        }                      
+                    </Cube>
                 </div>
             </div>
         </center>
